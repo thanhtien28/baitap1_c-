69fd632e4b0a923aab105a0c432ed06ad02cc1d3
thanh Tien <thanhtien5755933@gmail.com>
2026-01-27 04:22:22 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pham Thanh Tien");
+            Console.WriteLine("2415053122341");
+        }
+    }
+}
