a5b9ac94eb6b2106ded1aab8cdc14ad531c8560b
thanhtien28 <thanhtien5755933@gmail.com>
2026-03-02 14:48:21 +0700

    Add files via upload

---
+++ tuan4/mang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{   
+    public class mang<T>
+    {
+        public void travekdl()
+        {
+            Console.WriteLine(typeof(T));
+        }
+    }
+ }
