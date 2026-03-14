32c333c133c56965f1140772a5b9e01247cca072
thanh Tien <thanhtien5755933@gmail.com>
2026-03-15 00:25:08 +0700

    bai10

---
+++ Bai6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai6 = numbers.Count(x => x%2==0);
+
+Console.WriteLine("Co {0} so chan trong danh sach",bai6);
