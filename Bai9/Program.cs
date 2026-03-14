52e57cab2d95e0ca0ab2ae3033bcf686b3fe3841
thanh Tien <thanhtien5755933@gmail.com>
2026-03-15 00:14:06 +0700

    Bai12

---
+++ Bai9/Program.cs
@@ -0,0 +1,12 @@
+﻿using SinhVien;
+
+Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<Student> students = new List<Student>() {  new Student { Id = 1, Name = "An", Score = 8 },
+                                                new Student { Id = 2, Name = "Binh", Score = 6 },
+                                                new Student { Id = 3, Name = "Chi", Score = 9 },
+                                                new Student { Id = 4, Name = "Dung", Score = 7 } };
+var bai9 = students.Select(student => student.Name).ToList();
+
+Console.Write("Cac sinh vien gom: ");
+bai9.ForEach(x => Console.Write(x + ", "));
