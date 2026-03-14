6e5ddb96eed73b54459e655da9489970492d1d4d
thanh Tien <thanhtien5755933@gmail.com>
2026-03-15 00:21:48 +0700

    bai 18

---
+++ Bai15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<int> num = new List<int> { 1,2,2,3,4,4,5 };
+var bai15 = num.Distinct().ToList();
+
+Console.Write("Danh sach gom: ");
+bai15.ForEach(x=> Console.Write(x+" "));
---
+++ Bai17/Program.cs
@@ -0,0 +1,10 @@
+﻿using SinhVien;
+
+Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<Student> students = new List<Student>() {  new Student { Id = 1, Name = "An", Score = 8 }, 
+                                                new Student { Id = 2, Name = "Binh", Score = 6 }, 
+                                                new Student { Id = 3, Name = "Chi", Score = 9 }, 
+                                                new Student { Id = 4, Name = "Dung", Score = 7 }};
+var bai17 = students.Average(x => x.Score);
+Console.WriteLine("Diem trung binh sinh vien la: " + bai17);
