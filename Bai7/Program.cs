cab83e343e645df7fcad622a76144b827e2a37d2
thanh Tien <thanhtien5755933@gmail.com>
2026-03-15 00:08:06 +0700

    Bai9

---
+++ Bai7/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8};
+var bai7 = numbers.Any(x => x > 10);
+
+Console.WriteLine(bai7 ? "Co so lon hon 10" : "Khong co so lon hon 10");
