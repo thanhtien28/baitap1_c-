555309f538b643eb35793a6b5c5ae42883df9b17
thanh Tien <thanhtien5755933@gmail.com>
2026-03-15 00:31:22 +0700

    Bai1

---
+++ Bai1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai1 = numbers.Where(x => x%2 == 0).ToList();
+
+Console.Write("Danh sach so chan: ");
+bai1.ForEach(x => Console.Write(x + " "));
+
---
+++ Pham_Thanh_Tien_2415053122341.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenNamespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pham Thanh Tien ");
+            Console.WriteLine("MSV:2415053122341");
+        }
+    }
+}
