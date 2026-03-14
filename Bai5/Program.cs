939d7f3f1e7b2a9d83275d8bd26cb75c5b4327b9
thanh Tien <thanhtien5755933@gmail.com>
2026-03-15 00:07:15 +0700

    Bai8

---
+++ Bai5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai5 = numbers.Select(x => x * x).ToList();
+
+Console.Write("Danh sach binh phuong: ");
+bai5.ForEach(x=> Console.Write(x +" "));
