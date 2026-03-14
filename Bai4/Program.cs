08a1eb314ee4a104ab8565c22158c976a47a0971
thanh Tien <thanhtien5755933@gmail.com>
2026-03-15 00:04:43 +0700

    Bai5

---
+++ Bai4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("PHAM THANH TIEN\nMa sinh vien: 2415053122341\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai4 = numbers.OrderByDescending(x => x).ToList();
+
+Console.Write("Danh sach giam dan: ");
+bai4.ForEach(x => Console.Write(x + " "));
+
+
