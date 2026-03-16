445215898ed8ee616d1e1378ed230da4761b35d8
thanh Tien <thanhtien5755933@gmail.com>
2026-03-16 14:02:29 +0700

    Tuan 7

---
+++ tuan5/sinhvien.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tuan5
+{
+    public class sinhvien
+    {
+        public string ten { get; set; }
+        public int tuoi { get; set; }
+        public int namhoc { get; set; }
+        
+        public float  dtb { get; set; }
+        public string khoa { get; set; }
+        public sinhvien(string ten, int tuoi, int namhoc, float dtb, string khoa)
+        {
+            this.ten = ten;
+            this.tuoi = tuoi;
+            this.namhoc = namhoc;
+            this.dtb = dtb;
+            this.khoa = khoa;
+        }
+    }
+}
